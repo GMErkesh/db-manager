72c7da1d9a85a698878fd99f351dd239dfe38b27
Erkesh <erkesh@example.com>
2025-05-27 00:40:14 +0500

    Initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">DB Manager App</h1>
+    </div>
+  );
+}
+
+export default App;
